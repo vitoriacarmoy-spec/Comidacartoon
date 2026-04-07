de81b7358526c50a04e3db8cbbf23371fcc7e974
vitoriacarmoy-spec <vitoria.carmoy@gmail.com>
2026-04-07 17:28:47 -0300

    feature/div
    divs com animaçao

---
+++ dreamcore.js
@@ -0,0 +1,13 @@
+console.log("Funcionando!");
+
+setTimeout(() => {
+  document.querySelector(".barra").classList.add("mexer");
+}, 5000);
+
+setTimeout(() => {
+  document.querySelector(".barra").classList.add("mexer2");
+}, 10000);
+
+setTimeout(() => {
+  document.querySelector(".barra").classList.add("mexer3");
+}, 15000);
