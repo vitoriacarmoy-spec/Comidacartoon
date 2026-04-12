cdb6e347971343a94d1d9c15b336e763ac49bd99
vitoriacarmoy-spec <vitoria.carmoy@gmail.com>
2026-04-12 15:47:30 -0300

    gloriaadeus
    recuperei meus codigos jesus, deus e bom

--- dreamcore.js
+++ dreamcore.js
@@ -2,12 +2,51 @@ console.log("Funcionando!");
 
 setTimeout(() => {
   document.querySelector(".barra").classList.add("mexer");
-}, 5000);
+}, 50000);
 
 setTimeout(() => {
   document.querySelector(".barra").classList.add("mexer2");
-}, 10000);
+}, 100000);
 
 setTimeout(() => {
   document.querySelector(".barra").classList.add("mexer3");
-}, 15000);
+}, 150000);
+
+setTimeout(() => {
+  document.querySelector(".barra").classList.add("mexer4");
+}, 200000);
+
+setTimeout(() => {
+  document.querySelector(".barra").classList.add("mexer5");
+}, 250000);
+
+//separação bonitinha
+window.onload = () => {
+const img = document.querySelector(".falabarra")
+
+const imagens=[
+"2.png",
+"4.png",
+"6.png",
+"8.png",
+"10.png"
+
+];
+let i=0;
+const intervalo = setInterval(() =>{
+img.classList.add("sumir");
+
+setTimeout(() =>{
+  i++;
+  img.src=imagens[i];
+  img.classList.remove("sumir");
+
+  //
+  if(i===imagens.length - 1){
+    clearInterval(intervalo);
+  }
+},1000); //fadetime
+
+},5000);
+
+};
