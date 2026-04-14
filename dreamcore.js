437a6537a2cc3e035e7e9d85639734d5d5b58c3e
vitoriacarmoy-spec <vitoria.carmoy@gmail.com>
2026-04-13 23:16:15 -0300

    barra
    animacao e ajustes de imagem

--- dreamcore.js
+++ dreamcore.js
@@ -2,51 +2,86 @@ console.log("Funcionando!");
 
 setTimeout(() => {
   document.querySelector(".barra").classList.add("mexer");
-}, 50000);
+}, 5000);
 
 setTimeout(() => {
   document.querySelector(".barra").classList.add("mexer2");
-}, 100000);
+}, 10000);
 
 setTimeout(() => {
   document.querySelector(".barra").classList.add("mexer3");
-}, 150000);
+}, 15000);
 
 setTimeout(() => {
   document.querySelector(".barra").classList.add("mexer4");
-}, 200000);
+}, 20000);
 
 setTimeout(() => {
   document.querySelector(".barra").classList.add("mexer5");
-}, 250000);
+}, 25000);
 
 //separação bonitinha
 window.onload = () => {
-const img = document.querySelector(".falabarra")
 
-const imagens=[
-"2.png",
-"4.png",
-"6.png",
-"8.png",
-"10.png"
+  // FALABARRA
+  const img = document.querySelector(".falabarra");
 
-];
-let i=0;
-const intervalo = setInterval(() =>{
-img.classList.add("sumir");
+  const imagens = ["2.png","4.png","6.png","8.png","10.png"];
+  let i = 0;
 
-setTimeout(() =>{
-  i++;
-  img.src=imagens[i];
-  img.classList.remove("sumir");
+  const intervalo = setInterval(() => {
+    img.classList.add("sumir");
 
-  //
-  if(i===imagens.length - 1){
-    clearInterval(intervalo);
-  }
-},1000); //fadetime
+    setTimeout(() => {
+      i++;
+      img.src = imagens[i];
+       if (imagens[i] === "8.png") {
+      img.classList.add("subir");
+    } else {
+      img.classList.remove("subir");
+    }
+    if (imagens[i] === "10.png") {
+      img.classList.add("adireita");
+    } else {
+      img.classList.remove("adireita");
+    }
 
-},5000);
+
+      img.classList.remove("sumir");
+
+      if (i === imagens.length - 1) {
+        clearInterval(intervalo);
+      }
+    }, 1000);
+
+  }, 5000);
+
+
+  // REALBARRA
+  const img2 = document.querySelector(".realbarra");
+
+  const imagens2 = ["b1.png","b2.png","b3.png","b4.png","b5.png"];
+  let i2 = 0;
+
+  const intervalo2 = setInterval(() => {
+    img2.classList.add("sumir");
+
+    setTimeout(() => {
+      i2++;
+      img2.src = imagens2[i2];
+       if (imagens[i] === "10.png") {
+      img.classList.add("aesquerda");
+    } else {
+      img.classList.remove("aesquerda");
+    }
+
+      img2.classList.remove("sumir");
+
+      if (i2 === imagens2.length - 1) {
+        clearInterval(intervalo2);
+      }
+    }, 1000);
+
+  }, 5000);
 
 };
